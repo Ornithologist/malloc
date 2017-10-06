cffe74eca06534fcb329996ffa1b230d9456c1e0
Ornithologist <anthing1markmax@gmail.com>
2017-10-06 00:01:29 -0400

    types and main flow

--- common.h
+++ common.h
@@ -1,11 +1,54 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define BASE_BLOCK 8
 #define MIN_NUM_ARENA 2
-#define SIZE_TO_ORDER(size) ((int) ceil( (log(size) / log(BASE_BLOCK) )) )
+#define BASE_BLOCK_BYTES 32          // 32 B
+#define MMAP_THRESHOLD_BYTES 128000  // 128 kB
+#define SIZE_TO_ORDER(size) (ceil(log(ceil(size / BASE_BLOCK_BYTES)) / log(2)))
+#define BASE_ORDER SIZE_TO_ORDER(BASE_BLOCK_BYTES)
+#define MAX_ORDER SIZE_TO_ORDER(MMAP_THRESHOLD_BYTES)
+
+typedef struct _block_meta {
+    void *addr;
+    uint8_t order;
+    uint8_t allocated;
+    uint8_t mmaped;
+    union {
+        struct _block_meta *next;
+        void *data[0];
+    } block_data;
+} block_meta_t;
+
+typedef struct _arena_meta {
+    pthread_mutex_t lock;
+    block_meta_t *blocks[] unit16_t heap_count;
+    unit8_t thread_count;
+    size_t alloc_count;
+    size_t free_count;
+    size_t block_count;
+    block_meta_t *blocks[MAX_ORDER + 2];  // normal blocks plus one big block
+    int in_use;
+    struct _arena_meta *next;
+} arena_meta_t;
+
+typedef struct _heap_meta {
+    size_t size;
+    unit8_t mmapped;
+    arena_meta_t *arena_p;
+    struct _heap_meta *next;
+    void *content;
+} heap_meta_t;
+
+typedef struct _thread_meta {
+    arena_meta_t *arena_p;
+    heap_meta_t *heap_p;
+} thread_meta_t;
+
+block_meta_t *alloc_by_order(arena_meta_t *arena_p, uint8_t order);
 
 #endif
--- malloc.c
+++ malloc.c
@@ -4,28 +4,29 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "common.h"
+
+static int arena_count = 1;
+static int processor_count = 1;
+static int page_size = 4096;
+
+static __thread arena_meta_t* ts_arena_ptr;
+static __thread pthread_key_t arena_key;
+static thread_meta_t main_thread = { 0 };
+static pthread_mutex_t malloc_thread_init_lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *malloc(size_t size)
 {
-  // TODO: Validate size.
-  size_t allocSize = size + sizeof(MallocHeader);
-
-  void *ret = mmap(0, allocSize, PROT_READ | PROT_WRITE,
-                   MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-  assert(ret != MAP_FAILED);
-
-  // We can't use printf here because printf internally calls `malloc` and thus
-  // we'll get into an infinite recursion leading to a segfault.
-  // Instead, we first write the message into a string and then use the `write`
-  // system call to display it on the console.
-  char buf[1024];
-  snprintf(buf, 1024, "%s:%d malloc(%zu): Allocated %zu bytes at %p\n",
-           __FILE__, __LINE__, size, allocSize, ret);
-  write(STDOUT_FILENO, buf, strlen(buf) + 1);
-
-  MallocHeader *hdr = (MallocHeader*) ret;
-  hdr->size = allocSize;
-
-  return ret + sizeof(MallocHeader);
+    block_meta_t *block_p;
+    size_t size_needed = size + sizeof(block_meta_t);
+    uint8_t order_needed = SIZE_TO_ORDER(size_needed);
+    if (order_needed > MAX_ORDER) {
+        // use mmap
+    } else {
+        // use sbrk
+        if ((block_p = alloc_by_order(ts_arena_ptr, order_needed)) != NULL)
+            block_p->allocated = 1;
+    }
+    return (void *) block_p->block_data.data;
 }
